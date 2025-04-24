312157895fedab6344745f9f918e18f1311c9510
Sksham Kaushal <skshamkaushal@gmail.com>
2025-04-25 01:37:49 +0530

    DataBase

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const { error } = require('console');
+const mongoose =require('mongoose')
+const db=process.env.DB_URL;
+
+mongoose.connect(db).then(()=>{
+console.log('☠️☠️☠️☠️☠️');
+
+}).catch((error)=>{
+    console.log(error);
+    
+
+})
+
+module.exports=mongoose;
