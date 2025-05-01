72da5aba1d51fee386d2952632037cfb03684783
Sksham Kaushal <skshamkaushal@gmail.com>
2025-05-01 18:29:59 +0530

    Update workoutRoutes.js

--- backend/routes/workoutRoutes.js
+++ backend/routes/workoutRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
-const Workout = require('../models/Workout'); // ✅ Added missing import
+const Workout = require('../models/Workout'); 
 
 // POST
 
