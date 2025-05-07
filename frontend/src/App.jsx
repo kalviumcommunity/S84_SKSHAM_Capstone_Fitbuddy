3711c88daf718cda7f04f4398d2b4013c5acb533
Sksham Kaushal <skshamkaushal@gmail.com>
2025-05-07 22:57:43 +0530

    Add frontend components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import HeroSection from "../components/HeroSection";
+import FeatureCards from "../components/FeatureCards";
+import CallToAction from "../components/CallToAction";
+import Footer from "../components/Footer";
+
+function App() {
+  return (
+    <>
+      <HeroSection />
+      <FeatureCards />
+      <CallToAction />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
